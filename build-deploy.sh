14df8cd2614d4b3e50331ce3e5a267dfddf40edd
Tim <timkelsch@gmail.com>
2023-07-24 12:20:11 -0600

    Simplify URL Building

--- build-deploy.sh
+++ build-deploy.sh
@@ -2,12 +2,12 @@
 
 set -xeuo pipefail
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+#export NVM_DIR="$HOME/.nvm"
+#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
-nvm unload
+#nvm unload
 
-export AWS_PROFILE="sam-personal-dev"
+#export AWS_PROFILE="sam-personal-dev"
 SAM=$(which sam)
 
 $SAM build
