772694d419ba4c61c79304862d84852fced49679
timkelsch <timkelsch@gmail.com>
2023-10-27 14:27:01 -0600

    d

--- push.sh
+++ push.sh
@@ -2,33 +2,44 @@
 
 set -euxo pipefail
 
+REGISTRY="${AWS_ACCOUNT}.dkr.ecr.us-east-1.amazonaws.com"
+REPO="mfl-score"
+
 # Login
 aws ecr get-login-password --region us-east-1 | docker login --username AWS \
-  --password-stdin "${AWS_ACCOUNT}.dkr.ecr.us-east-1.amazonaws.com"
+  --password-stdin "${REGISTRY}"
 
 # Only works if there is one tag per image
 CURRENT_VERSION=$(aws ecr describe-images --region "${AWS_REGION}" --output json --repository-name mfl-score \
 --query 'sort_by(imageDetails,& imagePushedAt)[-1].imageTags[0]' | jq . -r)
+CURRENT_IMAGE="${REGISTRY}/${REPO}:${CURRENT_VERSION}"
 
 # TODO: Check if the image has changed so we're not just putting a new tag on an existing image
 
 IFS=. read -r v1 v2 <<< "${CURRENT_VERSION}"    # split into (integer) components
 ((v2++))                                        # do the math
-NEXT_VERSION="${v1}.${v2}"                      # paste back together
-# NEXT_VERSION=0.13
+# NEXT_VERSION="${v1}.${v2}"                      # paste back together
+NEXT_VERSION=0.15
 
-NEW_IMAGE_URI="${AWS_ACCOUNT}.dkr.ecr.us-east-1.amazonaws.com/mfl-score:${NEXT_VERSION}"
+NEW_IMAGE_URI="${REGISTRY}/${REPO}:${NEXT_VERSION}"
 
-NEW_IMAGE_SHA=$(docker build -q -t mfl-scoring-image:"${NEXT_VERSION}" . | cut -d: -f2)
+NEW_IMAGE_ID=$(docker build -q -t "${REPO}:${NEXT_VERSION}" . | cut -d: -f2)
 
-CURRENT_VERSION_SHA=$(aws ecr describe-images --repository-name mfl-score --image-ids imageTag="${CURRENT_VERSION}" | \
-  jq -r '.imageDetails[] | select(.imageTags[]=="'"${CURRENT_VERSION}"'") | .imageDigest' | cut -d: -f2)
+# Check if CURRENT_IMAGE already exists locally
+if ! CURRENT_IMAGE_ID=$(docker image ls "${CURRENT_IMAGE}"); then
+  docker pull "${CURRENT_IMAGE}"
+  if ! CURRENT_IMAGE_ID=$(docker inspect --format '{{.Id}}' "${CURRENT_IMAGE}"); then
+    echo "Cannot determine current image ID. Exiting."
+    exit
+  fi
+fi
 
-if [[ "${CURRENT_VERSION_SHA}" == "${NEW_IMAGE_SHA}" ]]; then
+if [[ "${CURRENT_IMAGE_ID}" == "${NEW_IMAGE_ID}" ]]; then
+  echo "The image built for this commit already exists in the remote repository. Exiting."
   exit
 fi
 
-docker tag "${NEW_IMAGE_SHA}" "${NEW_IMAGE_URI}"
+docker tag "${NEW_IMAGE_ID}" "${NEW_IMAGE_URI}"
 docker push "${NEW_IMAGE_URI}"
 
 aws lambda update-function-code --function-name "${FUNCTION_NAME}" --architectures arm64 \
